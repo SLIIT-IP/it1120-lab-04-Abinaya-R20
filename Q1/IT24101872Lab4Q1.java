ec686ef371dfd482f5121968d609905245f01986
Abinaya-R20 <IT24102078@my.sliit.lk>
2024-08-25 17:10:00 +0530

    IT24101872Lab4Q1.java

---
+++ Q1/IT24101872Lab4Q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT24101872Lab4Q1{
+public static void main (String [] args)
+{
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter a number : ");
+int number = scanner.nextInt();
+
+if (number < 0) { 
+System.out.println ("negative");}
+
+ else if  (number > 0)
+{ System.out.print (" positive" );}
+
+else { 
+System.out.print ("Zero");}
+
+
+}
+
+}
